1e333dc7aa5a9c1230ea5c58ffe6689c28ecf84b
Scott-PC\Scott <veracityit@gmail.com>
2018-03-11 02:26:21 +1100

    added socket.io support

---
+++ src/io.ts
@@ -0,0 +1,21 @@
+﻿import * as Express from 'express';
+//import * as httpServer from 'http';
+
+
+const app = new Express();
+//const http = 
+
+const router = new Router();
+
+router.get('/', async (ctx) => {
+    ctx.body = 'Sup!';
+    
+});
+
+app.use(router.routes());
+
+app.listen(3000, function () {
+    console.log('listening on *:3000');
+});
+
+console.log('Server running on port 3000');
